124644014e7918f3a380c8712a729463946e0c17
pauline_barnades <barnades.pauline@gmail.com>
2020-04-19 19:42:04 +0200

    Adding jobcard id#2, testing website link functionning when clicking in job card

--- cypress/integration/userCanSeeListOfJobs.feature.js
+++ cypress/integration/userCanSeeListOfJobs.feature.js
@@ -11,5 +11,16 @@ describe('User can see list of jobs', () => {
         cy.get('.description').should('contain', 'Class mentor for students grade 4 and 5.');
         cy.get('.link').should('exist')
       })
+
+      it('displays second most recent position', () => {
+        cy.get('#cv-2').within(() => {
+          cy.get(('.image')).should('exist');
+          cy.get('.ui.header').should('contain', 'Primary school teacher at Lycee francais of Dubai, UAE');
+          cy.get('.description').should('contain', 'Class mentor for students grade 4.');
+          cy.get('.link').should('exist')
+        })
+
+
+
     });
 });
--- src/JobCard.jsx
+++ src/JobCard.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import CV from "./cv";
 
-const JobCard = ({ cv }) =>{
+const JobCard = ({ cv }) => {
     return (
-        <>
-         <div class="ui card">
-         <div class="image">
-         <img src={CV.image} />
-         </div>
-         <div class="content">
-         <h3 class="ui header">{cv.name}</h3>
-         <div class="description">{cv.description}</div>
-         <div className="link"><a href={jobs.link}>schoolWebsite</a>
-         </div>
-         </div>
-       </div>
-       </>
-     );
-   };
-   export default JobCard;
+      <>
+        <div class="ui card">
+          <div class="image">
+            <img src={cv.image} />
+          </div>
+          <div class="content">
+            <h3 class="ui header"><a href={cv.link}> <h3 class="ui header">{cv.name}</h3> </a></h3>
+  
+            <div class="description">{cv.description}</div>  
+            <div class="link"><a href={cv.link}><h5>school's website</h5></a>
+
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+export default JobCard;
